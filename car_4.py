ab1887ec0336d5151f0c167830eeaa2413990ca8
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-07-01 10:05:05 +0300

    Add files via upload

---
+++ car_4.py
@@ -0,0 +1,40 @@
+# Let's write a class representing a car. 
+# This class will contain information about the type of machine, as well as a method for outputting a short description.
+
+class Car():
+    # A simple car model.
+    def __init__(self, make, model, year):
+        # Initializes the attributes of the vehicle description
+        self.make = make
+        self.model = model
+        self.year = year 
+        self.odometer_reading = 150
+        
+    def get_descriptive_name(self):
+        # Returns a neatly formatted description.
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+        
+    def read_odometer(self):
+        # Displays the mileage of the car in miles.
+        print(f"This car has {self.odometer_reading} miles on it.")
+        
+    def update_odometer(self, mileage):
+        # Sets the setpoint on the odometer. When trying to reverse spin, the change is rejected.
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+            
+    def increment_odometer(self, miles):
+        # Increases the odometer readings with the specified increment.
+        self.odometer_reading +=miles
+        
+my_used_car = Car("subaru", "outback", 2015)
+print(my_used_car.get_descriptive_name())
+
+my_used_car.update_odometer(23_500)
+my_used_car.read_odometer()
+
+my_used_car.increment_odometer(100)
+my_used_car.read_odometer()
